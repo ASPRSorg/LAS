fa8cff453bf1106c19793f4ac769401456e6d7be
Howard Butler <howard@hobu.co>
2017-02-12 16:57:23 -0600

    build_docs needs to use asprsorg/las docker image

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "building docs for $TRAVIS_BUILD_DIR"
-docker run -v $TRAVIS_BUILD_DIR:/data -w /data osgeo/proj.4 make latexpdf
+docker run -v $TRAVIS_BUILD_DIR:/data -w /data asprsorg/las make latexpdf
 
 
