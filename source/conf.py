307acab478f47c6788c4137530973150a0c73bcd
Evon Silvia <evon.silvia@nv5.com>
2025-01-16 13:32:43 -0800

    Update copyright year

--- source/conf.py
+++ source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'LAS'
-copyright = u'2024, ASPRS'
+copyright = u'2025, ASPRS'
 author = u'ASPRS'
 
 # The version info for the project you're documenting, acts as replacement for
