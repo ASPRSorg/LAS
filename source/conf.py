3d3c72bb4e3b7ca6b07b37459911368715fddb2f
Howard Butler <hobu.inc@gmail.com>
2025-04-06 15:59:46 -0500

    use rtd theme

--- source/conf.py
+++ source/conf.py
@@ -111,7 +111,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -142,7 +142,11 @@ preamble = r'''
 % https://stackoverflow.com/questions/4839105/sphinx-customization-of-latexpdf-output
 % bug: this still doesn't apply to the TOC and title page. not sure what style's there.
 
+\usepackage{charter}
+\usepackage[defaultsans]{lato}
+\usepackage{inconsolata}
 \usepackage{titling}
+
 \usepackage{fancyhdr}
 \makeatletter
 \fancypagestyle{normal}{
