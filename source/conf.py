04464abeaa2eba327e62dd1d8de01e990ee37fab
Howard Butler <hobu.inc@gmail.com>
2025-04-09 10:45:46 -0500

    use pydata theme

--- source/conf.py
+++ source/conf.py
@@ -111,7 +111,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -119,7 +119,7 @@ html_theme = 'sphinx_rtd_theme'
 #
 # HTML theme options in Alabaster here:
 # https://alabaster.readthedocs.io/en/latest/customization.html
-# Alabaster default CSS is here: 
+# Alabaster default CSS is here:
 # https://github.com/sphinx-doc/alabaster/blob/master/alabaster/static/alabaster.css_t
 
 # html_theme_options = {}
